b685bbef04efee6fc5777a313589ef01c99a139b
eglumov <eugen.glumov@yandex.ru>
2026-06-09 00:52:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,64 +2,54 @@
 #include <cstdint>
 #include "alg.h"
 
-
 double pown(double value, uint16_t n) {
-  if (n == 0)
-    return 1;
-  double result = 1;
-  for (uint16_t i = 1; i <= n; i++) {
-    result *= value;
-  }
-
-  return result;
+    double result = 1.0;
+    while (n > 0) {
+        result *= value;
+        n--;
+    }
+    return result;
 }
 
 uint64_t fact(uint16_t n) {
-  if (n == 0)
-    return 1;
-  uint64_t result = 1;
-
-  for (int i = 1; i <= n; i++) {
-    result *= i;
-  }
-
-  return result;
+    uint64_t result = 1;
+    while (n > 1) {
+        result *= n;
+        n--;
+    }
+    return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  return pown(x, n) / fact(n);
+    return pown(x, n) / static_cast<double>(fact(n));
 }
 
 double expn(double x, uint16_t count) {
-  double result = 0;
-  for (uint16_t i = 0; i <= count; i++) {
-    result += calcItem(x, i);
-  }
-  return result;
+    double sum = 0.0;
+    for (uint16_t i = 0; i < count; ++i) {
+        sum += calcItem(x, i);
+    }
+    return sum;
 }
 
 double sinn(double x, uint16_t count) {
-  double result = 0;
-  int y = 1;
-  for (uint16_t i = 0; i < count; i++) {
-    if (i % 2 == 0)
-      result += calcItem(x, y);
-    else
-      result -= calcItem(x, y);
-    y += 2;
-  }
-  return result;
+    double sum = 0.0;
+    double sign = 1.0;
+    for (uint16_t i = 0; i < count; ++i) {
+        uint16_t power = 2 * i + 1;
+        sum += sign * calcItem(x, power);
+        sign = -sign;
+    }
+    return sum;
 }
 
 double cosn(double x, uint16_t count) {
-  double result = 0;
-  int y = 0;
-  for (uint16_t i = 0; i < count; i++) {
-    if (i % 2 == 0)
-      result += calcItem(x, y);
-    else
-      result -= calcItem(x, y);
-    y += 2;
-  }
-  return result;
+    double sum = 0.0;
+    double sign = 1.0;
+    for (uint16_t i = 0; i < count; ++i) {
+        uint16_t power = 2 * i;
+        sum += sign * calcItem(x, power);
+        sign = -sign;
+    }
+    return sum;
 }
